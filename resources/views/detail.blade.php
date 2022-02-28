7172bf6d5a5287344633a4d8c15f5e34e6bc92ae
Gorbion1702 <jonathanjason125@gmail.com>
2022-02-28 18:52:50 +0700

    update home, about, course, user

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -23,7 +23,7 @@
     {{-- navbar --}}
     <nav class="navbar navbar-expand-lg navbar-light bg-info">
         <div class="container">
-          <a class="navbar-brand" href="#">LnT</a>
+          <a class="navbar-brand" href="#">CODER</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
